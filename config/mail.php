79583eaaf3c8003ca650a70d643b5a54b166ed15
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-12-29 14:10:21 -0800

    Fix

--- config/mail.php
+++ config/mail.php
@@ -30,7 +30,7 @@ return [
 	'mailers' => [
 		'smtp'     => [
 			'transport'  => 'smtp',
-			'host'       => env( 'MAIL_HOST', 'smtp-relay.google.com' ),
+			'host'       => env( 'MAIL_HOST', 'smtp-relay.gmail.com' ),
 			'port'       => env( 'MAIL_PORT', 587 ),
 			'encryption' => env( 'MAIL_ENCRYPTION', 'tls' ),
 			'username'   => env( 'MAIL_USERNAME' ),
